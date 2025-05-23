c08afb2c59d474b7bd7a6b7e3c2f3336d3d15bdd
st-Aminh <minh.le@stunited.vn>
2025-05-23 08:57:35 +0700

    feat [SCRUM-5] add page

---
+++ src/components/contact/contact_page.tsx
@@ -0,0 +1,10 @@
+import { LuVideo } from "react-icons/lu";
+
+export default function ContactPage() {
+  return (
+    <div
+      className="w-full h-[700px] bg-cover bg-center"
+      style={{ backgroundImage: "url('/bg-contact.png')" }}
+    />
+  );
+}
